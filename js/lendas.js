7af6e4147bd53a24a816979d970d0f7ff0a9e311
Vitor Aurilandio <132232270+VitorAuri@users.noreply.github.com>
2023-12-12 12:41:50 -0300

    Add files via upload

--- js/lendas.js
+++ js/lendas.js
@@ -3,7 +3,29 @@ const personagens = document.getElementById('personagens')
 const lendaNome = document.getElementById('lenda-p')
 const personagem = document.getElementById('personagem')
 
-personagens.addEventListener('change', function () {
+personagens.addEventListener('change', function(){
     lendaNome.innerHTML = personagens.options[personagens.selectedIndex].getAttribute("data-value")
     personagem.src =  personagens.options[personagens.selectedIndex].getAttribute("value")
 });
+
+const crossovers = document.getElementById('crossovers')
+
+crossovers.addEventListener('change', function(){
+    lendaNome.innerHTML = crossovers.options[crossovers.selectedIndex].getAttribute("data-value")
+    personagem.src = crossovers.options[crossovers.selectedIndex].getAttribute("value")
+})
+
+const ativarCrossovers = document.getElementById('ativarCrossovers')
+const desativarCrossovers = document.getElementById('desativarCrossovers')
+function ativar(){
+    ativarCrossovers.classList.add('display')
+    personagens.classList.add('display')
+    crossovers.classList.remove('display')
+    desativarCrossovers.classList.remove('display')
+}
+function desativar(){
+    ativarCrossovers.classList.remove('display')
+    personagens.classList.remove('display')
+    crossovers.classList.add('display')
+    desativarCrossovers.classList.add('display')
+}
